fc828d0b570e09f6d3c3a961ea40ddd0441b81a3
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-16 12:39:43 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,15 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+	
+	def new
+		@post = Post.new
+	end
+
+	def create
+	end
 
 	def show
 		@post = Post.find(params[:id])
-	end
-
-	def new
-		@post = Post.new
 	end
 end
