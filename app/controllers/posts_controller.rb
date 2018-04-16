d603c6cd04ec355b279205cb94ea4884e2b01ba3
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-16 12:43:43 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,20 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+	
+	def new
+		@post = Post.new
+	end
+
+	def create
+	  @post = Post.new
+	  @post.title = params["title"]
+	  @post.description = params["description"]
+	  @post.save
+	  redirect_to post_path(@post)
+	end
 
 	def show
 		@post = Post.find(params[:id])
-	end
-
-	def new
-		@post = Post.new
 	end
 end
